15a0dfdd2fbea97e6dbb91d7a13e7faba0d76a88
Parth Shah <parth95shah@users.noreply.github.com>
2015-06-07 11:41:16 +0530

    The master file
    This file will have the code that will run Snake game on your device

---
+++ MASTER.C
@@ -0,0 +1,35 @@
+#include<graphics.h>
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+   /* request auto detection */
+   int gdriver = DETECT, gmode, errorcode;
+   int xmax, ymax;
+
+   /* initialize graphics and local variables */
+   initgraph(&gdriver, &gmode, "");
+
+   /* read result of initialization */
+   errorcode = graphresult();
+   /* an error occurred */
+   if (errorcode != grOk)
+   {
+      printf("Graphics error: %s\n", grapherrormsg(errorcode));
+      printf("Press any key to halt:");
+      getch();
+      exit(1);
+   }
+
+   setcolor(getmaxcolor());
+   xmax = getmaxx();
+   ymax = getmaxy();
+
+   /* draw a diagonal line */
+   line(0, 0, xmax, ymax);
+
+   /* clean up */
+   getch();
+   closegraph();
+   return 0;
+}
