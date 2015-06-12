edb8c7aab8b9650a26c5dd382e69eb7a0838995e
Parth Shah <parth95shah@users.noreply.github.com>
2015-06-13 03:15:51 +0530

    Release 1.3 (Erratic Endings Solved!)
    Finally and hopefully, the erratic endings of the game have been solved.
    Do test it for yourself though. You can never be sure!

--- MASTER.C
+++ MASTER.C
@@ -1,9 +1,12 @@
+/*
+This code was created by Parth H. Shah
+This game can be freely copied and used anywhere if and only if the name of the creator, which is Parth H. Shah is included in the file
+This code is available on GitHub.
+*/
 #include<graphics.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
-//Release 1.2 is ready, but some bugs still remain unsolved... Some of them are:
-//Sometimes the game gets erratic where the first line grows indefinitely whereas the last line is drawn in a random manner
 int snakeStartX,snakeStartY,snakeEndX,snakeEndY;  //coordinates of the snake's start and end position
 int endGame = 0;  //set this flag if the game has ended
 int headDirection = 4;  //tells the direction in which the head of the snake is moving right now. 1=up, 2=down, 3=left, 4=right
@@ -16,13 +19,13 @@ int counter;  //acts as a counter for the loops
 int keyFlag=0;  //prevents rapid pressing of keys and facilitates processing
 char playerName[20];
 char keyPress;  //logs the key pressed by the player
-unsigned long int score = 0;
-void scoreCalc();
-void gameOver();
-void moveSnake();
-void drawSnake();
-void drawFood();
-void keyListener();
+unsigned long int score = 0;  //gives current score of the player
+void scoreCalc();  //will update the score whenever snake eats food
+void gameOver();  //will set the endGame parameter whenever the snake hits the borders or its own body
+void moveSnake();  //helps move the snake on the screen by updating all the parameters and turns
+void drawSnake();  //moves the snake by rapidly refreshing the screen and updating the new parameter values from moveSnake
+void drawFood();  //draws the food pixel on the screen randomly
+void keyListener();  //will listen for any keyboard input and do the appropriate action
 int main(void)
 {
 	int gdriver = DETECT, gmode, errorcode;
@@ -46,11 +49,11 @@ int main(void)
 	snakeStartY = getmaxy() / 2;
 	snakeEndX = snakeStartX - 50;
 	snakeEndY = snakeStartY;
+	//next four lines get the coordinates of the food on the screen, leaving 20 pixels from the borders unused. This can be removed in the patches
 	foodX = rand() % (getmaxx() - 40);
 	foodY = rand() % (getmaxy() - 40);
 	foodX += 20;
 	foodY += 20;
-	//above four lines get the coordinates of the food on the screen, leaving 20 pixels from the borders unused. This can be removed in the patches
 	while(endGame == 0)
 	{
 		//the whole code of running the game goes in here
@@ -72,6 +75,8 @@ int main(void)
 }
 void gameOver()
 {
+	//this else-if ladder will check for the color of the next pixel that the snake is going to go
+	//if that color is equal to the color of the snake's body, it's game over!
 	if(headDirection==1)
 	{
 		if(getpixel(snakeStartX,snakeStartY-1)==WHITE)
@@ -104,6 +109,7 @@ void gameOver()
 			return;
 		}
 	}
+	//another way to end the game is by touching any of the boundaries
 	if(snakeStartX >= getmaxx() || snakeStartX<=0 || snakeStartY>=getmaxy() || snakeStartY<=0)
 	{
 		endGame = 1;
@@ -112,6 +118,9 @@ void gameOver()
 }
 void moveSnake()
 {
+	keyFlag=1;  //ensure that no key is pressed while this process is taking place
+	//the two else-if ladders will increment or decrement the value of the coordinates of the startpoint and endpoint
+	//ladder 1
 	if(tailDirection==1)
 	{
 		snakeEndY--;
@@ -128,6 +137,8 @@ void moveSnake()
 	{
 		snakeEndX++;
 	}
+	//ladder 1 ends
+	//ladder 2
 	if(headDirection==1)
 	{
 		snakeStartY--;
@@ -144,6 +155,9 @@ void moveSnake()
 	{
 		snakeStartX++;
 	}
+	//ladder 2 ends
+	//ifthe endpoint coordinates coincide with the turn coordinates
+	//we need to change direction of end segment and swap the values to update them
 	if(snakeEndX == turnX[0] && snakeEndY==turnY[0])
 	{
 		tailDirection=turnDirection[0];
@@ -155,10 +169,13 @@ void moveSnake()
 		}
 		activeTurns--;
 	}
+	keyFlag=0;  //since the process has completed, you can safely press the keys now
 }
 void scoreCalc()
 {
 	//the else if ladder given below will update the score and increase the size of the snake. the break has been made to shorten the long condition
+	//if head coincides with any of the nine food pixels, increase its size, change its origin and update the score
+	//ladder
 	if((snakeStartX==foodX && snakeStartY==foodY) || (snakeStartX==foodX && snakeStartY==foodY-1) || (snakeStartX==foodX && snakeStartY==foodY+1))
 	{
 		if(headDirection==1)
@@ -234,11 +251,18 @@ void scoreCalc()
 		foodY += 20;
 		//above four lines get the coordinates of the food on the screen, leaving 20 pixels from the borders unused. This can be removed in the patches
 	}
+	//ladder ends
 }
 void drawSnake()
 {
+	//if there are no turns, draw a straight line between startpoint and endpoint
 	if(activeTurns == 0)
+	{
 		line(snakeStartX,snakeStartY,snakeEndX,snakeEndY);
+	}
+	//else, if there are 3 or more turns, draw a line between the endpoint and the first(oldest) turn
+	//then draw a line between every consecutive turn
+	//at the end draw a line between the last(latest) turn and the startpoint
 	else if(activeTurns > 2)
 	{
 		line(turnX[0],turnY[0],snakeEndX,snakeEndY);
@@ -249,12 +273,14 @@ void drawSnake()
 		}
 		line(snakeStartX,snakeStartY,turnX[activeTurns - 1],turnY[activeTurns - 1]);
 	}
+	//if there ar exactly two turns, proceed as above, but here the lines have been hardcoded
 	else if(activeTurns == 2)
 	{
 		line(turnX[0],turnY[0],snakeEndX,snakeEndY);
 		line(snakeStartX,snakeStartY,turnX[1],turnY[1]);
 		line(turnX[0],turnY[0],turnX[1],turnY[1]);
 	}
+	//else if there is only one turn, draw a line each from end to turn and turn to start
 	else if(activeTurns==1)
 	{
 		line(turnX[0],turnY[0],snakeEndX,snakeEndY);
@@ -263,27 +289,27 @@ void drawSnake()
 }
 void keyListener()
 {
-	if(kbhit() && keyFlag==0)  //a key has been pressed
+	if(kbhit() && keyFlag==0)  //a key has been pressed and that too not rapidly
 	{
-		keyFlag=1;
-		keyPress = getch();
-		if(keyPress == 27)
+		keyFlag=1;  //to avoid dummy key presses
+		keyPress = getch();  //get the ascii code
+		if(keyPress == 27)   //if ESC is pressed, end the game
 		{
 			endGame = 1;
 			return;
 		}
 		if(keyPress == 0)  //it's an arrow key
 		{
-			keyPress = getch();
-			activeTurns++;
-			turnX[activeTurns-1]=snakeStartX;
-			turnY[activeTurns-1]=snakeStartY;
+			keyPress = getch();  //get the scan code
 			if(keyPress==72)
 			{
 				if(headDirection==2)
 				{
 					return;
 				}
+				activeTurns++;
+				turnX[activeTurns-1]=snakeStartX;
+				turnY[activeTurns-1]=snakeStartY;
 				headDirection=1;
 				turnDirection[activeTurns-1]=1;
 			}
@@ -293,6 +319,9 @@ void keyListener()
 				{
 					return;
 				}
+				activeTurns++;
+				turnX[activeTurns-1]=snakeStartX;
+				turnY[activeTurns-1]=snakeStartY;
 				headDirection=2;
 				turnDirection[activeTurns-1]=2;
 			}
@@ -302,6 +331,9 @@ void keyListener()
 				{
 					return;
 				}
+				activeTurns++;
+				turnX[activeTurns-1]=snakeStartX;
+				turnY[activeTurns-1]=snakeStartY;
 				headDirection = 3;
 				turnDirection[activeTurns - 1] = 3;
 			}
@@ -311,6 +343,9 @@ void keyListener()
 				{
 					return;
 				}
+				activeTurns++;
+				turnX[activeTurns-1]=snakeStartX;
+				turnY[activeTurns-1]=snakeStartY;
 				headDirection = 4;
 				turnDirection[activeTurns - 1] = 4;
 			}
@@ -323,7 +358,7 @@ void keyListener()
 }
 void drawFood()
 {
-	//next nine lines draw food for our snake
+	//next nine lines draw food for our snake, on the pixel and the immediate neighbouring pixels
 	putpixel(foodX-1,foodY-1,BLUE);
 	putpixel(foodX+1,foodY-1,BLUE);
 	putpixel(foodX+1,foodY+1,BLUE);
